69b2f99133554b9ef508efe47799d62e02115970
Orchid <ovaldez@appdev.year.org>
2025-04-13 12:37:09 -0400

    first commit

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,6 +2,34 @@ package com.ps;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        double principal = 2500.0;
+        double annualInterestRate = 0.0275;
+        int matureInterestYears = 10;
+
+
+        // Calculate total number of days  //
+        double numberOfDays = matureInterestYears * 365;
+
+        // Calculate daily interest rate //
+        double dailyInterestRate =  annualInterestRate / 365;
+
+        // Calculate the future value formula //
+        double futureValue = principal * Math.pow(1 + dailyInterestRate, numberOfDays);
+
+        // Calculate total interest earned //
+        double totalInterestEarned = futureValue - principal;
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
