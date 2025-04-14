9e9df7671e6fd172c14984dc20bd5eb8b9c01de9
Orchid <ovaldez@appdev.year.org>
2025-04-13 20:23:58 -0400

    first commit

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -7,20 +7,32 @@ public class Main {
         double annualInterestRate = 0.0275;
         int matureInterestYears = 10;
 
+        String dailyCompound = "daily"; // dailyCompound represents how often interest is compounded //
+        double futureValue = 0.0;
 
-        // Calculate total number of days  //
-        double numberOfDays = matureInterestYears * 365;
+        switch ("") {
+            case "daily":
+                futureValue = principal * Math.pow(1 + (annualInterestRate / 365), 365 * matureInterestYears);
+                break;
+            case "monthly":
+                futureValue = principal * Math.pow(1 + (annualInterestRate / 12), 12 * matureInterestYears);
+                break;
+            case "yearly":
+                futureValue = principal * Math.pow(1 + annualInterestRate, matureInterestYears);
+                break;
+            default:
+                System.out.println("Daily payment is invalid.");
+                futureValue = principal * Math.pow(1 + (annualInterestRate / 365), (365 * matureInterestYears));
+                break;
+        }
 
-        // Calculate daily interest rate //
-        double dailyInterestRate =  annualInterestRate / 365;
+        double interestEarned = futureValue - principal;
 
-        // Calculate the future value formula //
-        double futureValue = principal * Math.pow(1 + dailyInterestRate, numberOfDays);
-
-        // Calculate total interest earned //
-        double totalInterestEarned = futureValue - principal;
-
-
+        System.out.printf("Original Deposit: $% .2f%n", principal);
+        System.out.printf("Annual Interest Rate: % .4f%%%n", annualInterestRate * 100);
+        System.out.printf("Years of Interest: %d%n", matureInterestYears);
+        System.out.printf("Ending Balance: $% .2f%n", futureValue);
+        System.out.printf("Total Interest Earned: $% .2f%n", interestEarned);
 
 
 
